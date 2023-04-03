0936ea1901fed07668579d455f40782cb7e30c30
iurilugao01 <128332733+iurilugao01@users.noreply.github.com>
2023-04-03 19:46:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
 let inputDebito = document.getElementById('debito')
 let saldo = document.getElementById('saldo')
 
-if(localStorage.getItem('saldo') !== null) {
+  if(parseFloat(inputDebito.value) = NaN ) {
+    inputDebito.value = "0"
+  }
+  
+let entrada = document.getElementById('entrada')
+let saida = document.getElementById('saida')
+
+  if(localStorage.getItem('saldo') !== null) {
     saldo.innerText = localStorage.getItem("saldo")
   }
-
-mudarCorDoSaldo()
+  if(localStorage.getItem('entrada') !== null) {
+    entrada.innerText = localStorage.getItem("entrada")
+  }
+  if(localStorage.getItem('saida') !== null) {
+    saida.innerText = localStorage.getItem("saida")
+  }
 
 function pagar() {
     let saldoNumber = parseFloat(saldo.innerText)
     let debitoNumber = parseFloat(inputDebito.value)
 
+    saida.innerText = String(inputDebito.value)
+    localStorage.setItem("saida", saida.innerText)
+
     if(inputDebito.value !== "" && !isNaN(debitoNumber)) {
         saldo.innerText = String(saldoNumber - debitoNumber);
       }
-    
-      
+
+     
     localStorage.setItem("saldo", saldo.innerText)
     inputDebito.value = null
-    mudarCorDoSaldo()
+    
     
 }
 function receber() {
     let saldoNumber = parseFloat(saldo.innerText)
-    let debitoNumber = parseFloat(inputDebito.value) 
+    let debitoNumber = parseFloat(inputDebito.value)
+
+    entrada.innerText = String(inputDebito.value)
+    localStorage.setItem("entrada", entrada.innerText)
 
     if(inputDebito.value !== "" && !isNaN(debitoNumber)) {
         saldo.innerText = String(saldoNumber + debitoNumber);
       }
-    
+
     localStorage.setItem("saldo", saldo.innerText)
-    inputDebito = null
-    mudarCorDoSaldo()
+    inputDebito.value = null
 }
-function mudarCorDoSaldo() {
-    if (saldo.innerText[0] === "-") {
-        saldo.style.color = "red"
-    } else {
-        saldo.style.color = "green"
-    }
-}
-function reset() {
-    localStorage.setItem("saldo", 50000)
-    saldo.innerText = 50000
-}
+
+
 
 
 
